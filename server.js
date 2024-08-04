80946080da32d9b08b9d493d9294f62ad3138afb
squidword928 <113092158+squidword928@users.noreply.github.com>
2024-08-05 01:24:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,26 @@ app.get('/api/daily-verse', (req, res) => {
     res.json({ verse: verses[index] });
 });
 
+// Route for Daily Verse page
+app.get('/daily-verse', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/daily-verse.html'));
+});
+
+// Route for Gallery page
+app.get('/gallery', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/gallery.html'));
+});
+
+// Route for Dates to Remember page
+app.get('/dates-to-remember', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/dates-to-remember.html'));
+});
+
+// Catch-all route to handle 404 errors
+app.use((req, res) => {
+    res.status(404).send('404: NOT_FOUND\nCode: NOT_FOUND\nID: sin1::cpsfm-1722792123200-16fc2c16aa63');
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
